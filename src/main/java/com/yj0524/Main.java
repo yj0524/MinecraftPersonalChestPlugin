36d981faddf93292f3417857e8365479d5111882
yj0524 <jeonyunjae_kr@naver.com>
2023-02-15 12:45:34 +0900

    1.0.0

---
+++ src/main/java/com/yj0524/Main.java
@@ -0,0 +1,91 @@
+package com.yj0524;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+
+public class Main extends JavaPlugin implements Listener {
+
+    private HashMap<Player, Inventory> chests;
+
+    @Override
+    public void onEnable() {
+        // Register events
+        getServer().getPluginManager().registerEvents(this, this);
+        // Initialize chests map
+        chests = new HashMap<>();
+        getLogger().info("Plugin Enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        // Clean up chests map
+        chests.clear();
+        getLogger().info("Plugin Disabled.");
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        if (chests.containsKey(event.getPlayer())) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage("You cannot break your personal chest!");
+        }
+    }
+
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent event) {
+        if (chests.containsKey(event.getPlayer())) {
+            // Save chest contents to config
+            Player player = (Player) event.getPlayer();
+            Inventory chest = chests.get(player);
+            for (int i = 0; i < chest.getSize(); i++) {
+                ItemStack item = chest.getItem(i);
+                if (item != null) {
+                    getConfig().set(player.getUniqueId() + "." + i, item);
+                } else {
+                    getConfig().set(player.getUniqueId() + "." + i, null);
+                }
+            }
+            saveConfig();
+            // Remove chest from map
+            chests.remove(player);
+        }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (cmd.getName().equalsIgnoreCase("personalchest")) {
+                if (chests.containsKey(player)) {
+                    // Open personal chest
+                    player.openInventory(chests.get(player));
+                } else {
+                    // Create personal chest
+                    Inventory chest = Bukkit.createInventory(null, 27, "Personal Chest");
+                    // Load chest contents from config
+                    for (int i = 0; i < chest.getSize(); i++) {
+                        if (getConfig().contains(player.getUniqueId() + "." + i)) {
+                            ItemStack item = getConfig().getItemStack(player.getUniqueId() + "." + i);
+                            chest.setItem(i, item);
+                        }
+                    }
+                    chests.put(player, chest);
+                    player.openInventory(chest);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+}
