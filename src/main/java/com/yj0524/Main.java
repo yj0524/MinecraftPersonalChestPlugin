400b2966d7ad27d942dad24989fc82e7a154c825
yj0524 <jeonyunjae_kr@naver.com>
2023-02-16 22:50:54 +0900

    1.0.1

--- src/main/java/com/yj0524/Main.java
+++ src/main/java/com/yj0524/Main.java
@@ -1,7 +1,6 @@
 package com.yj0524;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
